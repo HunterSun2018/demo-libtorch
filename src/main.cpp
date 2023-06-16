af0960d3fdad6233e4b0b6a76f8d50962956a979
Hunter Sun <HunterSun2018@gmail.com>
2023-06-16 15:00:59 +0800

    handle argument 1 to determinate if run with GUP, default : true

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,33 @@
 #include <iostream>
+#include <sstream>
+#include <chrono>
 #include <torch/torch.h>
 #include <ATen/cuda/CUDAContext.h>
 using namespace std;
 
 
 void print_CUDA_cuDNN_info();
+int run_mnist(bool usingGPU = true);
 
 int main(int argc, char* argv[])
 {
+    bool usingGPU = true;
+
+    if (argc >= 2)
+        istringstream(argv[1]) >> std::boolalpha >> usingGPU;
+
     cout << "cuda is " << torch::cuda::is_available() << endl;
 
     print_CUDA_cuDNN_info();
+
+    auto start = std::chrono::steady_clock::now();
+
+    run_mnist(usingGPU);
+
+    auto end = std::chrono::steady_clock::now();
+    chrono::duration<double> elapsed_seconds = end - start;
+
+    std::cout << "elapsed time: " << elapsed_seconds.count() << "s\n";
 
     return 0;
 }
